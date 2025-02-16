00f1f5a4201de46d67305e75062e844ba1941f25
brian <78578795+ap-actual@users.noreply.github.com>
2025-02-16 12:19:19 -0500

    Fix calculations in 2.2.1

--- Module2/ModNav_2.2.1.py
+++ Module2/ModNav_2.2.1.py
@@ -1,35 +1,43 @@
-import math
-
+from math import cos
+from math import sin
+from math import acos
+from math import radians
+from math import pi
 
 # coordinates from problem
-x1 = [39, 77]
-x2 = [39, 100]
+x0 = [39, -77]
+x1 = [39, -100]
 
-# radius of earth, in mi
-re = 3963.1
+# radius of earth, in nmi
+re = 3443.8
 
-# get small circle radius via re_small = re * sin(lat)
-re_small_circle = re * math.sin(math.radians(x1[0]))
+# get small circle radius via re_small = re * cos(lat)
+re_small_circle = re * cos(radians(x0[0]))
 
 # get distance in longitude, in rads
-theta = math.radians(x2[1] - x1[1])
+theta = radians(x1[1] - x0[1])
 
 # compute small circle distance
 dist_small_circle = re_small_circle * theta
+print("---------------\nsmall circle dist (nmi):")
 print(dist_small_circle)
 
 # calculate ECEF position of each coordinate
-X1_X = re * math.cos(math.radians(x1[0])) * math.cos(math.radians(x1[1]))
-X1_Y = re * math.cos(math.radians(x1[0])) * math.sin(math.radians(x1[1]))
-X1_Z = re * math.sin(math.radians(x1[0]))
+X0 = [0, 0, 0]
+X0[0] = re * cos(radians(x0[0])) * cos(radians(x0[1]))
+X0[1] = re * cos(radians(x0[0])) * sin(radians(x0[1]))
+X0[2] = re * sin(radians(x0[0]))
 
-X2_X = re * math.cos(math.radians(x2[0])) * math.cos(math.radians(x2[1]))
-X2_Y = re * math.cos(math.radians(x2[0])) * math.sin(math.radians(x2[1]))
-X2_Z = re * math.sin(math.radians(x2[0]))
+X1 = [0, 0, 0]
+X1[0] = re * cos(radians(x1[0])) * cos(radians(x1[1]))
+X1[1] = re * cos(radians(x1[0])) * sin(radians(x1[1]))
+X1[2] = re * sin(radians(x1[0]))
 
 # compute great circle distance
-x_dot_prod = (X1_X * X2_X) + (X1_Y * X2_Y) + (X1_Z * X2_Z)
-theta      = math.acos(x_dot_prod / (re * re))
+x_dot_prod = (X0[0] * X1[0]) + (X0[1] * X1[1]) + (X0[2] * X0[2])
+theta      = acos(x_dot_prod / (re * re))
 
 dist_great_circle = theta * re
+
+print("---------------\ngreat circle dist (nmi):")
 print(dist_great_circle)
